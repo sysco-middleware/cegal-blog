edb000b43e5bd630df1afeb008d789232e31e98a
JGH153 <jan.greger@gmail.com>
2022-09-28 10:25:58 +0200

    correct footer url

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
   return (
     <footer className="mt-5 text-center p-5 justify-center items-center border-t-2 ">
       <p>© {year} - Jan Greger Hemb</p>
-      <a href="https://curious-programming.sanity.studio/desk">Sanity Studio</a>
+      <a href="https://cegal-blog.sanity.studio/desk/">Sanity Studio</a>
     </footer>
   );
 }
